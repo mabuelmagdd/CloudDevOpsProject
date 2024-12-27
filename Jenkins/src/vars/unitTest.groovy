9393ae8f9051e9569939bd884a489a79fc73aa48
mabuelmagdd <93723941+mabuelmagdd@users.noreply.github.com>
2024-12-27 20:44:42 +0200

    Update unitTest.groovy

--- Jenkins/src/vars/unitTest.groovy
+++ Jenkins/src/vars/unitTest.groovy
@@ -1,13 +1,9 @@
 def call() {
     // This function will run unit tests using Gradle in a Jenkins pipeline.
     echo "Running Unit Tests..."
-
-    try {
         // Run unit tests using Gradle wrapper
-        sh 'chmod +x gradlew'
-        sh './gradlew test'
-    } catch (Exception e) {
-        currentBuild.result = 'FAILURE'
-        throw e
-    }
+       sh '''
+        chmod +x ./gradlew
+        ./gradlew test
+        '''
 }
