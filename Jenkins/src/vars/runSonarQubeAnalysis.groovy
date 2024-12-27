fa7098a7c61791b1c1b168ea0f61cb06b03a74e4
mabuelmagdd <93723941+mabuelmagdd@users.noreply.github.com>
2024-12-27 21:18:07 +0200

    Update runSonarQubeAnalysis.groovy

--- Jenkins/src/vars/runSonarQubeAnalysis.groovy
+++ Jenkins/src/vars/runSonarQubeAnalysis.groovy
@@ -1,23 +1,10 @@
-def call() {
-    // Get the EC2 Instance IP by querying AWS CLI or using any method that returns the IP address
-    def ec2Ip = sh(script: 'curl http://169.254.169.254/latest/meta-data/public-ipv4', returnStdout: true).trim()
-    
-    // Construct the SonarQube URL using the EC2 public IP
-    def sonarQubeUrl = "http://${ec2Ip}:9000"
-    
-    // Store the SonarQube URL in the environment variable
-    env.SONARQUBE_URL = sonarQubeUrl
-
-    // Display the EC2 public IP address and SonarQube URL
-    echo "The EC2 public IP address is: ${ec2Ip}"
-    echo "SonarQube URL is: ${env.SONARQUBE_URL}"
-
+def call(String sonarProjectKey, String sonarHostUrl, String sonarLogin) {
     // Run the SonarQube analysis
     withSonarQubeEnv('SonarQube') {
         sh """
-        ./gradlew sonarqube -Dsonar.projectKey=cloudDevOps-project \
-                              -Dsonar.host.url=${SONARQUBE_URL} \
-                              -Dsonar.login=${SONARQUBE_TOKEN}
+        ./gradlew sonarqube -Dsonar.projectKey=${sonarProjectKey} \
+                              -Dsonar.host.url=${sonarHostUrl} \
+                              -Dsonar.login=${sonarLogin}
         """
     }
 }
